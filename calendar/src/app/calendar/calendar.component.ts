46c8cc167da759d4db1b1d6a230241b922520b7f
Kateryna Ueno <katerynaueno@gmail.com>
2024-11-10 10:14:41 +0000

    signals calendar

--- calendar/src/app/calendar/calendar.component.ts
+++ calendar/src/app/calendar/calendar.component.ts
@@ -22,9 +22,8 @@ import { Meetings } from './meetings.interface';
 })
 
 export class CalendarComponent{
-
   meetings: InputSignal<Meetings> = input.required();
-  activeDay: WritableSignal<DateTime | null> = signal(null);
+  activeDay: WritableSignal<DateTime| null> = signal(null);
   today: Signal<DateTime> = signal(DateTime.now());
   firstDateOfActiveMonth: WritableSignal<DateTime> = signal(this.today().startOf('month'));
   weekDays : Signal<string[]> = signal(Info.weekdays('long'));
@@ -39,9 +38,58 @@ export class CalendarComponent{
     return day.start;
   })
 })
-
+    activeDayMeetings: Signal<string[]> = computed(() => {
+    const activeDay = this.activeDay();
+    if (activeDay === null) {
+      return []
+    }
+    const activeDayISO = activeDay.toISODate();
+     if (!activeDayISO){
+      return [];
+    }
+    return this.meetings()[activeDayISO] ?? [];
+  });
 
 DATE_MED = DateTime.DATE_MED;
+
+
+  // meetings: InputSignal<Meetings> = input.required();
+  // today: Signal<DateTime> = signal(DateTime.local());
+  // firstDayOfActiveMonth: WritableSignal<DateTime> = signal(
+  //   this.today().startOf('month'),
+  // );
+  // activeDay: WritableSignal<DateTime | null> = signal(null);
+  // weekDays: Signal<string[]> = signal(Info.weekdays('long'));
+  // daysOfMonth: Signal<DateTime[]> = computed(() => {
+  //   return Interval.fromDateTimes(
+  //     this.firstDayOfActiveMonth().startOf('week'),
+  //     this.firstDayOfActiveMonth().endOf('month').endOf('week'),
+  //   )
+  //     .splitBy({ day: 1 })
+  //     .map((d) => {
+  //       if (d.start === null) {
+  //         throw new Error('Wrong dates');
+  //       }
+  //       return d.start;
+  //     });
+  // });
+  // DATE_MED = DateTime.DATE_MED;
+
+  // activeDayMeetings: Signal<string[]> = computed(() => {
+  //   const activeDay = this.activeDay();
+  //   if (activeDay === null) {
+  //     return [];
+  //   }
+  //   const activeDayISO = activeDay.toISODate();
+
+  //   if (!activeDayISO) {
+  //     return [];
+  //   }
+
+  //   return this.meetings()[activeDayISO] ?? [];
+  // });
+
+
 
 constructor() {
   console.log(this.daysOfMonth())
